c3b7c015083dea0b010ceebbac875146991b146a
alleguedaiana <60580424+alleguedaiana@users.noreply.github.com>
2020-02-05 20:20:49 -0300

    Add files via upload
    Ejercicios hechos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var Descuento;
+
+    importe= document.getElementById("importe").value;
+    importe=parseInt(importe);
+
+    descuento= importe-((importe*25)/100);
+
+    document.getElementById("resultado").value= descuento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,12 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+var nombre;
+//cuando se trata de guardar el valor de una caja de texto traida por html se le coloca .value
+nombre= document.getElementById("elNombre").value;
+
+alert(nombre);
+
 }
 
 
