c079f9aa5a3c2e5c1db8ca5477b2943038ee1ceb
alumno <alumno>
2020-02-07 12:23:27 -0300

    ejercicios clase 2

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad=parseInt(document.getElementById("edad").value);
+
+if(edad>=18)
+    {
+        alert("Sos mayor de edad");
+    }
+else(edad<18)
+    {
+        alert("Sos menor de edad");
+    }
 
 }//FIN DE LA FUNCIÓN
