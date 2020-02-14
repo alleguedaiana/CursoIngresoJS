892e72ce1393c3ab3c641ddb69ec30739f676b74
alumno <alumno>
2020-02-14 12:11:11 -0300

    ejercicios switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,31 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+switch(mes)
+{
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
 
+        alert("Este mes tiene 31 días");
+        break;
 
+    case "Febrero":
+
+        alert("Este mes tiene 28 días");
+        break;
+
+    default:
+
+        alert("Este mes tiene 30 días");
+
+}
 
 }//FIN DE LA FUNCIÓN
