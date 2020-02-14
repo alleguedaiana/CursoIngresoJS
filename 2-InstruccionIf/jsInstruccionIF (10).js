2d3fb7c60607848502748adb9f689880548361ed
alumno <alumno>
2020-02-14 09:17:04 -0300

    Ejercicios If terminados

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	var maximo=10;
+	var minimo=1;
+
+	numero=Math.floor(Math.random()*((maximo+1)-minimo)+ minimo);
+
+	if(numero>=9)
+	{
+		alert("Su nota es "+numero+ ". Excelente");
+	}
+	else if(numero>=4)
+	{
+		alert("Su nota es "+numero+" . Aprobo");
+	}
+	else
+	{
+		alert("Su nota es "+numero+ " . Vamos, la proxima se puede");	
+	}
 
 }//FIN DE LA FUNCIÓN
