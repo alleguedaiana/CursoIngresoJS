59af0be71c9afbeb76d47af4d1c0a22cb20675d3
alleguedaiana <daianaallegue@gmail.com>
2020-02-14 21:28:08 -0300

    ejercicio 3 practica

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,11 @@ function mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while (clave != "utn750")
+{
+    clave= prompt("Ingrese la clave nuevamente");
+}
+
+alert("Contraseña correcta")
 
 }//FIN DE LA FUNCIÓN
