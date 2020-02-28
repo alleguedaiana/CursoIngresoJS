55dbf3de3937d7dd64364465dffd9d10572e7e2a
alumno <alumno>
2020-02-28 11:59:29 -0300

    done

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,6 @@
-function mostrar()
+//CUALQUIERA DE LAS DOS FORMAS, TANTO CON WHILE COMO CON FOR ESTÀN BIEN. LO RECOMENDADO ES FOR.
+
+/*function mostrar()
 {
     var numero;
     var notas=0;
@@ -39,6 +41,52 @@ function mostrar()
     }
 
     promedio= notas/contador;
+    alert("El promedio de las notas totales son: "+ promedio 
+            +"\n La nota más baja es: " + notaminima 
+            + "y su sexo " + sexonotaminima
+            +"\n La cantidad de varones con nota igual o mayor a 6 es: "+contadorvaron);
+
+
+}*/
+
+function mostrar()
+{
+    var numero;
+    var notas=0;
+    var sexo;
+    var promedio;
+    var notaminima;
+    var sexonotaminima;
+    var contadorvaron=0;
+
+    for (i=0; i<5; i++)
+    {
+        numero= parseInt(prompt("Ingresar una nota entre 0 y 10"));
+        while (numero < 0 || numero > 10 || isNaN(numero))
+        {
+            numero= parseInt(prompt("Nota incorrecta. Ingrese nuevamente"));
+        }
+
+        sexo= prompt("Ingresar f o m según sexo del alumno").toLowerCase();
+        while (sexo != "m" && sexo != "f")
+        {
+            sexo= prompt("Sexo incorrecto. Por favor, ingresar nuevamente").toLowerCase();
+        }
+
+        if (i== 0 || numero <notaminima)
+        {
+            notaminima=numero;
+            sexonotaminima=sexo;
+        }
+
+        if (sexo=="m" && numero >= 6)
+        {
+            contadorvaron=contadorvaron+1;
+        }
+        notas= notas + numero;
+    }
+
+    promedio= notas/i;
     alert("El promedio de las notas totales son: "+ promedio 
             +"\n La nota más baja es: " + notaminima 
             + "y su sexo " + sexonotaminima
